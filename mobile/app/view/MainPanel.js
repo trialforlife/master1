44ca7e46205169208484fd3a98ddeb0de6a72e80
trial4life <trialforlife@gmail.com>
2013-08-09 22:06:50 +0300

    menu+ cinema list works

--- mobile/app/view/MainPanel.js
+++
@@ -1,9 +0,0 @@
-Ext.define("SenchaNote.view.MainPanel", {
-	extend: "Ext.tab.Panel",
-	xtype: "mainpanel",
-
-	config: {
-		tabBarPosition: "bottom",
-		items: [ {xtype: "notelistcontainer"} , { xtype: "aboutpanel"} ]
-	}
-});
