f28b3d38604bcada31256e6649a918edfba9d6f0
trial4life <trialforlife@gmail.com>
2013-08-20 18:55:36 +0300

    s

--- mobile/app/view/Main.js
+++ mobile/app/view/Main.js
@@ -52,7 +52,7 @@ Ext.define('ListItem', {
 
             proxy: {
                 type: 'jsonp',
-                url: 'http://now/catlist.php',
+                url: 'http://now-yakutsk.stairwaysoft.net/catlist.php',
                 reader: {
                     type: 'json',
                     rootProperty: 'cat',
