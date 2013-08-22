c822b842c58d949cf649a0daf39986055467fa1a
trial4life <trialforlife@gmail.com>
2013-08-22 12:43:19 +0300

    vbox

--- filmlist.php
+++ filmlist.php
@@ -18,14 +18,12 @@ if (mysql_affected_rows() > 0) {
 	while($row = mysql_fetch_array($dbresult))
 	{
 		array_push($result["films"],array(
-			"id"=>$row["f_id"],
+			"id"=>addslashes((string)$row["f_id"]),
 			"name"=>addslashes((string)$row["f_name"]),
 			"image"=>addslashes((string)$row["f_image"]),
 			"time"=>addslashes((string)$row["f_time"]),
-			"filmpage"=>
-			//"<img style=\"width:500px; float:left ; height:50px;\" src=http://now/".$row["cb_banner"]."> <br><br>". 
-			$row["f_name"]."<img style=\"width:100px; float:right ; height:50px;\" src=http://now/".$row["f_image"]."><br>".$row["f_time"]."</br><small>".$row["f_content"]."</small></br>",
-			));
+			"filmpage"=>addslashes((string)$row["f_name"]."<img style='width:100px; float:right ; height:50px;' src=http://now/".$row["f_image"]."><br>".$row["f_time"]."</br><small>".$row["f_content"]."</small></br>",
+			);
 			
 	}
 
