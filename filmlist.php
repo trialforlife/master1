9d8c45d551fb421e09c81076333958105a30d353
trial4life <trialforlife@gmail.com>
2013-08-22 13:01:06 +0300

    aaa

--- filmlist.php
+++ filmlist.php
@@ -22,7 +22,7 @@ if (mysql_affected_rows() > 0) {
 			"name"=>addslashes((string)$row["f_name"]),
 			"image"=>addslashes((string)$row["f_image"]),
 			"time"=>addslashes((string)$row["f_time"]),
-			"filmpage"=>addslashes(string)$row["f_name"]."<img style='width:100px; float:right ; height:50px;' src=http://now/".$row["f_image"]."><br>".$row["f_time"]."</br><small>".$row["f_content"]."</small></br>")),
+			"filmpage"=>addslashes(string)$row["f_name"]."<img style=\"width:100px; float:right ; height:50px;\" src=http://now/".$row["f_image"]."><br>".$row["f_time"]."</br><small>".$row["f_content"]."</small></br>")),
 
 			//"<img style=\"width:500px; float:left ; height:50px;\" src=http://now/".$row["cb_banner"]."> <br><br>". 
 			));
