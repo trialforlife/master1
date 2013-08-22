739415c8d73c596f27f0c9164edd61d53711069a
trial4life <trialforlife@gmail.com>
2013-08-22 21:30:24 +0300

    ev++++

--- eventfilmlist.php
+++ eventfilmlist.php
@@ -7,25 +7,28 @@ mysql_set_charset('utf8' ,  $link);
 
 mysql_select_db("now-yakutsk", $link);
 
-$result = array("cinema"=>array());
+$f_cid = $_GET["f_cid"];
+$cc = 1;
+$result = array("films"=>array());
 
-$query = 'select * from `theatre` ';//left join `cinema_banner` on (cinema.c_id=cinema_banner.c_id)';
+$query = "select * from events where events.ev_id = '$f_cid' ";
 $dbresult = mysql_query($query);
 
 if (mysql_affected_rows() > 0) {
 	while($row = mysql_fetch_array($dbresult))
 	{
-		array_push($result["cinema"],array(
-			"cid"=>$row["t_id"],
-			"name"=>addslashes((string)$row["t_name"]),
-			"image"=>addslashes((string)$row["t_image"]),
-			"adress"=>addslashes((string)$row["t_adress"]),
-			"published"=>addslashes((string)$row["t_published"]),
-			"list"=>addslashes((string)$row["t_name"]).'<br>'.addslashes((string)$row["t_adress"]),
-			"img_full"=> "<img src=http://now/".addslashes((string)$row["t_image"]).">",
+		array_push($result["films"],array(
+			"id"=>$row["ev_id"],
+			"name"=>addslashes((string)$row["ev_name"]),
+			"image"=>addslashes((string)$row["ev_image"]),
+			"time"=>addslashes((string)$row["ev_time"]),
+			"filmpage"=>
+			//"<img style=\"width:500px; float:left ; height:50px;\" src=http://now/".$row["cb_banner"]."> <br><br>". 
+			$row["ev_name"]."<img style=\"width:100px; float:right ; height:50px;\" src=http://now/".$row["ev_image"]."><br>".$row["ev_time"]."</br><small>".$row["ev_content"]."</small></br>",
 			));
 			
 	}
+
 }
 
 mysql_close();
@@ -38,4 +41,5 @@ else {
 	header("Content-Type: application/x-json");
 	echo json_encode($result);
 }
+
 ?>
