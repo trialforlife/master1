9658954264f023f2c89c70370587629f3f5b80bd
trial4life <trialforlife@gmail.com>
2013-08-23 05:04:08 +0300

    structure

---
+++ frontmodel/shipmentfilmlist.php
@@ -0,0 +1,45 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: value=Origin, X-Requested-With, Content-Type, Accept');
+
+$link = mysql_connect("localhost","now-yakutsk","E327D28999");
+mysql_set_charset('utf8' ,  $link);
+
+mysql_select_db("now-yakutsk", $link);
+
+$f_cid = $_GET["f_cid"];
+$cc = 1;
+$result = array("films"=>array());
+
+$query = "select * from shipment_special where shipment_special.s_id = '$f_cid' ";
+$dbresult = mysql_query($query);
+
+if (mysql_affected_rows() > 0) {
+	while($row = mysql_fetch_array($dbresult))
+	{
+		array_push($result["films"],array(
+			"id"=>$row["ss_id"],
+			"name"=>addslashes((string)$row["ss_name"]),
+			"image"=>addslashes((string)$row["ss_image"]),
+			"time"=>addslashes((string)$row["ss_time"]),
+			"filmpage"=>
+			//"<img style=\"width:500px; float:left ; height:50px;\" src=http://now/".$row["cb_banner"]."> <br><br>". 
+			$row["ss_title"]."<br><img style=\"width:100px; float:left ; height:50px;\" src=http://now/".$row["ss_image"].">".$row["ss_name"]."<br>".$row["ss_time"]."</br><small>".$row["ss_content"]."</small></br>".$row["ss_price"],
+			));
+			
+	}
+
+}
+
+mysql_close();
+
+if (isset($_REQUEST["callback"])) {
+	header("Content-Type: text/javascript");
+	echo $_REQUEST["callback"]. "(" .json_encode($result). ");";
+}
+else {
+	header("Content-Type: application/x-json");
+	echo json_encode($result);
+}
+
+?>
---
+++ frontmodel/shipmentlist.php
@@ -0,0 +1,41 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: value=Origin, X-Requested-With, Content-Type, Accept');
+
+$link = mysql_connect("localhost","now-yakutsk","E327D28999");
+mysql_set_charset('utf8' ,  $link);
+
+mysql_select_db("now-yakutsk", $link);
+
+$result = array("cinema"=>array());
+
+$query = 'select * from `shipment` ';//left join `cinema_banner` on (cinema.c_id=cinema_banner.c_id)';
+$dbresult = mysql_query($query);
+
+if (mysql_affected_rows() > 0) {
+	while($row = mysql_fetch_array($dbresult))
+	{
+		array_push($result["cinema"],array(
+			"cid"=>$row["s_id"],
+			"name"=>addslashes((string)$row["s_name"]),
+			"image"=>addslashes((string)$row["s_image"]),
+			"adress"=>addslashes((string)$row["s_adress"]),
+			"published"=>addslashes((string)$row["s_published"]),
+			"list"=>"<div>".addslashes((string)$row["s_name"])."<br><small>".$row["s_type"]."</small><img style=\"width:50px; float:right ; height:20px;\" src=http://now/".$row["s_image"]."><br><div>",
+			"img_full"=> "<img src=http://now/".addslashes((string)$row["s_image"]).">",
+			));
+			
+	}
+}
+
+mysql_close();
+
+if (isset($_REQUEST["callback"])) {
+	header("Content-Type: text/javascript");
+	echo $_REQUEST["callback"]. "(" .json_encode($result). ");";
+}
+else {
+	header("Content-Type: application/x-json");
+	echo json_encode($result);
+}
+?>
