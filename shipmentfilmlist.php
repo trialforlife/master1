294abe887d755a47591febeed19850e573a741df
trial4life <trialforlife@gmail.com>
2013-08-23 03:46:41 +0300

    shss

--- shipmentfilmlist.php
+++ shipmentfilmlist.php
@@ -11,20 +11,20 @@ $f_cid = $_GET["f_cid"];
 $cc = 1;
 $result = array("films"=>array());
 
-$query = "select * from shipment where shipment.s_id = '$f_cid' ";
+$query = "select * from shipment_special where shipment_special.s_id = '$f_cid' ";
 $dbresult = mysql_query($query);
 
 if (mysql_affected_rows() > 0) {
 	while($row = mysql_fetch_array($dbresult))
 	{
 		array_push($result["films"],array(
-			"id"=>$row["s_id"],
-			"name"=>addslashes((string)$row["s_name"]),
-			"image"=>addslashes((string)$row["s_image"]),
-			"time"=>addslashes((string)$row["s_time"]),
+			"id"=>$row["ss_id"],
+			"name"=>addslashes((string)$row["ss_name"]),
+			"image"=>addslashes((string)$row["ss_image"]),
+			"time"=>addslashes((string)$row["ss_time"]),
 			"filmpage"=>
 			//"<img style=\"width:500px; float:left ; height:50px;\" src=http://now/".$row["cb_banner"]."> <br><br>". 
-			$row["s_name"]."<img style=\"width:100px; float:right ; height:50px;\" src=http://now/".$row["s_image"]."><br>".$row["s_time"]."</br><small>".$row["s_content"]."</small></br>",
+			$row["ss_title"]."<br>".$row["ss_name"]."<img style=\"width:100px; float:right ; height:50px;\" src=http://now/".$row["ss_image"]."><br>".$row["ss_time"]."</br><small>".$row["ss_content"]."</small></br>".$row["ss_price"].,
 			));
 			
 	}
