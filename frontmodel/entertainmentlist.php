f2db36fbcecd2ce8fc45ee0c3fb2a78618e1d9d6
trial4life <trialforlife@gmail.com>
2013-09-03 11:40:25 +0300

    Signed-off-by: trial4life <trialforlife@gmail.com>

--- frontmodel/entertainmentlist.php
+++ frontmodel/entertainmentlist.php
@@ -21,9 +21,10 @@ if (mysql_affected_rows() > 0) {
 			"image"=>addslashes((string)$row["en_image"]),
 			"adress"=>addslashes((string)$row["en_adress"]),
 			"published"=>addslashes((string)$row["en_published"]),
-			"list"=>"<div>".addslashes((string)$row["en_name"])."<br><small>".$row["en_type"]."</small><img style=\"width:50px; float:right ; height:20px;\" src=http://now/".$row["en_image"]."><br><div>",
+			//"list"=>"<div>".addslashes((string)$row["en_name"])."<br><small>".$row["en_type"]."</small><img style=\"width:50px; float:right ; height:20px;\" src=http://now/".$row["en_image"]."><br><div>",
 			"img_full"=> "<img src=http://now/".addslashes((string)$row["en_image"]).">",
-			));
+            "list"=>'<div class="nav-element1"><span class="txt">'.addslashes((string)$row["en_name"]).'</span><span class="r_arrow"></span><span class="location">'.addslashes((string)$row["en_adress"]).'</span><span class="img_box"><img style="width:200px; float:right ; height:138px;" src=http://now-yakutsk.stairwaysoft.net/mobile/img/'.$row["en_image"].'></span></div>',
+        ));
 			
 	}
 }
