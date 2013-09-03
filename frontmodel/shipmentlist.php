2b2e8de8fc7f7f8e565511c1b8784dd3b84be6a3
trial4life <trialforlife@gmail.com>
2013-09-03 17:40:30 +0300

    ++sh

--- frontmodel/shipmentlist.php
+++ frontmodel/shipmentlist.php
@@ -19,11 +19,16 @@ if (mysql_affected_rows() > 0) {
 			"cid"=>$row["s_id"],
 			"name"=>addslashes((string)$row["s_name"]),
 			"image"=>addslashes((string)$row["s_image"]),
-			"adress"=>addslashes((string)$row["s_adress"]),
+            "adress"=>addslashes((string)$row["r_adress"]),
+            "site"=>addslashes((string)$row["r_site"]),
+            "banner"=>addslashes((string)$row["r_banner"]),
+            "phone"=>addslashes((string)$row["r_phone"]),
+            "special"=>addslashes((string)$row["r_special"]),
 			"published"=>addslashes((string)$row["s_published"]),
 			//"list"=>"<div>".addslashes((string)$row["s_name"])."<br><small>".$row["s_type"]."</small><img style=\"width:50px; float:right ; height:20px;\" src=http://now/".$row["s_image"]."><br><div>",
 			"img_full"=> "<img src=http://now/".addslashes((string)$row["s_image"]).">",
-            "list"=>'<div class="nav-element1"><span class="txt">'.addslashes((string)$row["s_name"]).'</span><span class="r_arrow"></span><span class="location">'.addslashes((string)$row["s_adress"]).'</span><span class="img_box"><img style="width:200px; float:right ; height:138px;" src=http://now-yakutsk.stairwaysoft.net/mobile/img/'.$row["s_image"].'></span></div>',
+            //"list"=>'<div class="nav-element1"><span class="txt">'.addslashes((string)$row["s_name"]).'</span><span class="r_arrow"></span><span class="location">'.addslashes((string)$row["s_adress"]).'</span><span class="img_box"><img style="width:200px; float:right ; height:138px;" src=http://now-yakutsk.stairwaysoft.net/mobile/img/'.$row["s_image"].'></span></div>',
+            "list"=>'<div class="nav-element1"><span class="txt">'.addslashes((string)$row["s_name"]).'</span><span class="s_arrow"></span><span class="location">'.addslashes((string)$row["s_adress"]).'</span><span class="img_box"><img style="width:200px; float:right ; height:138px;" src=http://now-yakutsk.stairwaysoft.net/mobile/img/'.$row["s_image"].'></span></div>',
 
         ));
 			
