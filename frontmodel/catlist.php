4db2f4388dc5e08f9c6285fa6845993f4ccdd80c
trial4life <trialforlife@gmail.com>
2013-09-05 15:32:34 +0300

    +dis

--- frontmodel/catlist.php
+++ frontmodel/catlist.php
@@ -16,20 +16,23 @@ if (mysql_affected_rows() > 0) {
 	while($row = mysql_fetch_array($dbresult))
 	{   if($row["cat_id"]== 11){
         $cc='';
+        $d_style= '';
         }
         elseif(($row["cat_count"])!= 0 )
         {
             $cc = $row["cat_count"];
+            $d_style = 'color: rgba(255,255,255,0.3)';
         }
 
         else{
+            $d_style= '';
             $cc = '<img style="margin-right: -14px;" src="http://now-yakutsk.stairwaysoft.net/mobile/img/clock-ico.png">';
         }
 		array_push($result["cat"],array(
 			"id"=>$row["cat_id"],
             "c_count"=>addslashes((string)$row["cat_count"]),
             "name"=>addslashes((string)$row["cat_title"]),
-			"title"=>'<div class="nav-element"><span class="txt">'.addslashes((string)$row["cat_title"]).'</span><span class="calc">'.$cc.'</span></div>',
+			"title"=>'<div style="'.$d_style.'" class="nav-element"><span class="txt">'.addslashes((string)$row["cat_title"]).'</span><span class="calc">'.$cc.'</span></div>',
 			"code"=>addslashes((string)$row["cat_code"]
 			)));
 			
