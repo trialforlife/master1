d43b0a765f309fa22286d5261c09113acbd1f5cc
trial4life <trialforlife@gmail.com>
2013-09-09 16:11:53 +0300

    ++

--- frontmodel/restaurantlist.php
+++ frontmodel/restaurantlist.php
@@ -9,7 +9,7 @@ mysql_select_db("now-yakutsk", $link);
 
 $result = array("cinema"=>array());
 
-$query = 'select * from `restaurant` ';//left join `cinema_banner` on (cinema.c_id=cinema_banner.c_id)';
+$query = 'select * from restaurant ';//left join `cinema_banner` on (cinema.c_id=cinema_banner.c_id)';
 $dbresult = mysql_query($query);
 
 if (mysql_affected_rows() > 0) {
@@ -28,7 +28,7 @@ if (mysql_affected_rows() > 0) {
 //			"list"=>"<div>".addslashes((string)$row["r_name"])."<img style=\"width:50px; float:right ; height:20px;\" src=http://now/".$row["r_image"]."><br><div>",
 			"img_full"=> "<img src=http://now/".addslashes((string)$row["r_image"]).">",
 
-            "list"=>'<div class="nav-element1"><span class="txt">'.addslashes((string)$row["r_name"]).'</span><span class="r_arrow"></span><span class="location">'.addslashes((string)$row["r_adress"]).'</span><span class="img_box"><img style="width:200px; float:right ; height:138px;" src=http://now-yakutsk.stairwaysoft.net/mobile/img/'.$row["r_image"].'></span></div>',
+            "list"=>'<div class="nav-element1"><span class="txt">'.addslashes((string)$row["r_name"]).'</span><span class="r_arrow"></span><span class="location">'.addslashes((string)$row["r_adress"]).'</span><span class="img_box"><img style="width:20%; margin-top:2%; float:right ; height:90%;" src=http://now-yakutsk.stairwaysoft.net/mobile/img/'.$row["r_image"].'></span></div>',
 
         ));
 			
