d3cc91b6e511a2074748e162830f27c02ed3e932
trial4life <trialforlife@gmail.com>
2013-09-12 12:46:04 +0300

    Signed-off-by: trial4life <trialforlife@gmail.com>

--- protected/modules/backend/controllers/CinemaBannerController.php
+++ protected/modules/backend/controllers/CinemaBannerController.php
@@ -1,6 +1,6 @@
 <?php
 
-class cinemabannerController extends Controller
+class CinemaBannerController extends Controller
 {
 	/**
 	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
