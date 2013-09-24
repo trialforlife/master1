a21a0dff14a054ea01d34b50d6f89b19e07e08c0
trial4life <trialforlife@gmail.com>
2013-09-24 20:37:51 +0300

    Signed-off-by: trial4life <trialforlife@gmail.com>

--- frontmodel/beautyandhealhfilmlist.php
+++ frontmodel/beautyandhealhfilmlist.php
@@ -11,7 +11,7 @@ $f_cid = $_GET["f_cid"];
 $cc = 1;
 $result = array("films"=>array());
 
-$query = "select * from beautyandhealth_special where beautyandhealth_special.bh_id = '$f_cid' ";
+$query = "select bhs_id,bhs_name,bhs_image,bhs_time,bhs_content from beautyandhealth_special where beautyandhealth_special.bh_id = '$f_cid' ";
 $dbresult = mysql_query($query);
 
 if (mysql_affected_rows() > 0) {
@@ -20,7 +20,7 @@ if (mysql_affected_rows() > 0) {
 		array_push($result["films"],array(
 			"id"=>$row["bhs_id"],
 			"name"=>addslashes((string)$row["bhs_name"]),
-			"image"=>addslashes((string)$row["bhs_image"]),
+			//"image"=>addslashes((string)$row["bhs_image"]),
 			"time"=>addslashes((string)$row["bhs_time"]),
             "filmpage"=>'<div style="float: left;width: 97%;padding-left: 1.5%; padding-right: 0.5%;"><span class="h2">'.$row["bhs_name"]."</span><span class='time'>".$row["bhs_time"]."</span><div class='f_con'><img style=\"width:200px; float:right ; height:140px; margin-right:-1.5%;\" src=http://now-yakutsk.stairwaysoft.net/mobile/img/".$row["bhs_image"]."><pf>".$row["bhs_content"]."</pf></div></div>",
 
