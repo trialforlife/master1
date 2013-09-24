c57724129cb511fae1069c8b651d74c04d8c081e
trial4life <trialforlife@gmail.com>
2013-09-24 19:12:06 +0300

    Signed-off-by: trial4life <trialforlife@gmail.com>

--- mobile/connection.php
+++ mobile/connection.php
@@ -1,1 +1,5 @@
-<?php echo ("true"); ?>
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: value=Origin, X-Requested-With, Content-Type, Accept');
+echo ("true");
+?>
